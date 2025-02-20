4eaaf7246413c3d3695a94ea88ee732d93645c02
Tom <tomvandeusen@protonmail.com>
2025-02-20 08:47:03 +0700

    start gui implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod timer;
-fn main() {
-    println!("Hello, world!");
+
+use eframe::egui;
+use timer::Timer;
+
+fn main() -> eframe::Result {
+    tracing_subscriber::fmt::init();
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Stopwatch",
+        options,
+        Box::new(|cc| Box::<Content>::default()),
+    )
+}
+
+#[derive(Default)]
+struct Content {
+    timer: Timer,
+}
+
+impl eframe::App for Content {
+    fn update(
+        &mut self,
+        ctx: &egui::Context,
+        _frame: &mut eframe::Frame,
+    ) -> Option<eframe::ExitCode> {
+        self.timer.update(ctx);
+        None
+    }
 }
--- src/timer.rs
+++ src/timer.rs
@@ -1,34 +1,68 @@
+use std::fmt::Display;
 use std::time::{Duration, Instant};
 
+use tracing::instrument;
+
+#[derive(Debug)]
 enum TimerState {
     Stopped,
     Paused,
     Running,
 }
 
-struct Timer {
+#[derive(Debug)]
+pub struct Timer {
     state: TimerState,
     start_time: Option<Instant>,
-    accumulated: Duration,
-    current_time: Instant,
+    accumulated_time: Option<Duration>,
 }
 
 impl Timer {
+    #[instrument(name = "new_timer")]
     pub fn new() -> Self {
         Timer {
             state: TimerState::Stopped,
             start_time: None,
-            accumulated: Duration::new(0, 0),
+            accumulated_time: None,
         }
     }
 
+    #[instrument]
     pub fn start(&mut self) {
         self.state = TimerState::Running;
-        self.start_time = Some(Instant::now() + Instant::from(self.accumulated));
+        self.start_time = Some(Instant::now());
+        self.accumulated_time = Some(Duration::ZERO);
     }
 
+    #[instrument]
     pub fn pause(&mut self) {
         self.state = TimerState::Paused;
-        self.start_time = self.start_time.unwrap() + self.accumulated;
+        self.accumulated_time = Some(Instant::now().duration_since(self.start_time.unwrap()));
+    }
+
+    #[instrument]
+    pub fn unpause(&mut self) {
+        self.state = TimerState::Running;
+        self.start_time = Some(Instant::now());
+        self.accumulated_time = Some(Duration::new(0, 0));
+    }
+
+    #[instrument]
+    pub fn stop(&mut self) {
+        self.state = TimerState::Stopped;
+        self.start_time = None;
+        self.accumulated_time = None;
+    }
+}
+
+impl Display for Timer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let elapsed = self.accumulated_time.unwrap()
+            + Instant::now().duration_since(self.start_time.unwrap());
+        // write!(f, "{}", elapsed.as_millis())
+        let hours = elapsed.as_secs() / 3600;
+        let minutes = elapsed.as_secs() / 60 % 60;
+        let seconds = elapsed.as_secs() % 60;
+        write!(f, "{:02}:{:02}:{:02}", hours, minutes, seconds)
     }
 }
